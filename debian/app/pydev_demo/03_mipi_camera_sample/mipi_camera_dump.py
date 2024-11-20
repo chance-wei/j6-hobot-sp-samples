2a6407b240bcf725c92db48d25d3c70f384dabda
chenxu.wei <chenxu.wei@horizon.cc>
2024-11-20 13:17:43 +0800

    add pure camera sample

---
+++ debian/app/pydev_demo/03_mipi_camera_sample/mipi_camera_dump.py
@@ -0,0 +1,19 @@
+import sys, os, time
+sys.path.append("/usr/lib")
+import libsrcampy
+
+if __name__ == '__main__':
+    cam = libsrcampy.Camera()
+    ret = cam.open_cam(0, -1, 30, 1080, 1920, 1920, 1080)
+    a = 0
+    while a < 10:
+        time.sleep(3)
+        img = cam.get_img(2)
+        outimg = 'output%d.yuv' % a
+        fo = open(outimg, "wb")
+        fo.write(img)
+        fo.close()
+        print("write image success count: %d" % a)
+        a = a + 1
+
+    cam.close_cam()
